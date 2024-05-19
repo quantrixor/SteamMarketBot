b3580da49156cbdf3564c0abb1bd54baf50a84b2
Abdulkhakim Magomedov <quantrixor@gmail.com>
2024-05-19 23:16:25 +0300

    Нет информации о предмете, возможно не правильный ID

--- SteamMarketBot/Program.cs
+++ SteamMarketBot/Program.cs
@@ -3,47 +3,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SteamKit2;
-using SteamKit2.GC;
-using SteamKit2.GC.CSGO.Internal;
-using SteamKit2.Internal;
+using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Concurrent;
-using System.Linq;
+using System.Net.Http;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
 
 public class Program
 {
-    static SteamClient steamClient;
-    static CallbackManager manager;
-    static SteamUser steamUser;
-    static SteamGameCoordinator gameCoordinator;
-    static bool isRunning = false;
-    static uint APPID = 730; // CS:GO App ID
-
-    static ConcurrentDictionary<string, TaskCompletionSource<ItemDetails>> pendingRequests = new ConcurrentDictionary<string, TaskCompletionSource<ItemDetails>>();
+    static HttpClient httpClient = new HttpClient();
+    static string steamApiKey = "4C93431B3DB7CEA9C550DBBE65F3A784"; // Замените на ваш Steam API ключ
 
     public static void Main(string[] args)
     {
-        steamClient = new SteamClient();
-        manager = new CallbackManager(steamClient);
-
-        steamUser = steamClient.GetHandler<SteamUser>();
-        gameCoordinator = steamClient.GetHandler<SteamGameCoordinator>();
-
-        manager.Subscribe<SteamClient.ConnectedCallback>(OnConnected);
-        manager.Subscribe<SteamClient.DisconnectedCallback>(OnDisconnected);
-        manager.Subscribe<SteamUser.LoggedOnCallback>(OnLoggedOn);
-        manager.Subscribe<SteamUser.LoggedOffCallback>(OnLoggedOff);
-        manager.Subscribe<SteamGameCoordinator.MessageCallback>(OnGCMessage);
-
-        isRunning = true;
-
-        Console.WriteLine("Connecting to Steam...");
-        steamClient.Connect();
-
         var host = Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
@@ -88,119 +60,7 @@ public class Program
             })
             .Build();
 
-        var steamThread = new Thread(() =>
-        {
-            while (isRunning)
-            {
-                manager.RunWaitCallbacks(TimeSpan.FromSeconds(1));
-            }
-        })
-        {
-            IsBackground = true
-        };
-        steamThread.Start();
-
         host.Run();
-    }
-
-    static void OnConnected(SteamClient.ConnectedCallback callback)
-    {
-        Console.WriteLine("Connected to Steam! Logging in...");
-
-        steamUser.LogOn(new SteamUser.LogOnDetails
-        {
-            Username = "Cofdu2336",
-            Password = "RqKRmY0m2b366l",
-        });
-    }
-
-    static void OnDisconnected(SteamClient.DisconnectedCallback callback)
-    {
-        Console.WriteLine("Disconnected from Steam");
-        isRunning = false;
-
-        // Попробуем переподключиться после разрыва связи
-        Thread.Sleep(5000); // Задержка перед переподключением
-        steamClient.Connect();
-    }
-
-    static void OnLoggedOn(SteamUser.LoggedOnCallback callback)
-    {
-        if (callback.Result != EResult.OK)
-        {
-            Console.WriteLine($"Unable to log in to Steam: {callback.Result} / {callback.ExtendedResult}");
-            isRunning = false;
-            return;
-        }
-
-        Console.WriteLine("Successfully logged on!");
-
-        // Подключаемся к Game Coordinator
-        var playGames = new ClientMsgProtobuf<CMsgClientGamesPlayed>(EMsg.ClientGamesPlayed);
-        playGames.Body.games_played.Add(new CMsgClientGamesPlayed.GamePlayed
-        {
-            game_id = new GameID(APPID)
-        });
-        steamClient.Send(playGames);
-
-        Thread.Sleep(5000); // Задержка для установки соединения с GC
-        gameCoordinator.Send(new ClientGCMsgProtobuf<SteamKit2.GC.Artifact.Internal.CMsgClientHello>((uint)EGCBaseClientMsg.k_EMsgGCClientHello), APPID);
-    }
-
-    static void OnLoggedOff(SteamUser.LoggedOffCallback callback)
-    {
-        Console.WriteLine($"Logged off of Steam: {callback.Result}");
-    }
-
-    static void OnGCMessage(SteamGameCoordinator.MessageCallback callback)
-    {
-        var msg = callback.Message;
-        Console.WriteLine($"Received GC message: {msg.MsgType}");
-
-        if (msg.MsgType == (uint)ECsgoGCMsg.k_EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockResponse)
-        {
-            Console.WriteLine("Received Client2GCEconPreviewDataBlockResponse message.");
-
-            var response = new ClientGCMsgProtobuf<CMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockResponse>(msg);
-            var instanceId = response.Body.iteminfo.itemid.ToString(); // используем itemid как идентификатор экземпляра
-
-            Console.WriteLine($"InstanceId: {instanceId}");
-
-            if (pendingRequests.TryRemove(instanceId, out var tcs))
-            {
-                var itemDetails = new ItemDetails
-                {
-                    FloatValue = response.Body.iteminfo.paintwear,
-                    PaintSeed = (int)response.Body.iteminfo.paintseed,
-                    PaintIndex = (int)response.Body.iteminfo.paintindex,
-                    Stickers = response.Body.iteminfo.stickers.Select(s => new Sticker
-                    {
-                        Name = s.sticker_id.ToString(), // Используем sticker_id, так как поле name отсутствует
-                        Wear = s.wear
-                    }).ToArray()
-                };
-
-                Console.WriteLine($"FloatValue: {itemDetails.FloatValue}, PaintSeed: {itemDetails.PaintSeed}, PaintIndex: {itemDetails.PaintIndex}");
-                foreach (var sticker in itemDetails.Stickers)
-                {
-                    Console.WriteLine($"Sticker Name: {sticker.Name}, Wear: {sticker.Wear}");
-                }
-
-                tcs.SetResult(itemDetails);
-            }
-            else
-            {
-                Console.WriteLine("Failed to find pending request for instanceId.");
-            }
-        }
-        else if (msg.MsgType == (uint)EGCBaseClientMsg.k_EMsgGCClientWelcome)
-        {
-            Console.WriteLine("Received ClientWelcome message.");
-        }
-        else
-        {
-            Console.WriteLine($"Received unhandled GC message: {msg.MsgType}");
-        }
     }
 
     private static string? ParseInstanceIdFromLink(string link)
@@ -222,27 +82,46 @@ public class Program
 
     private static async Task<ItemDetails?> FetchItemDetails(string instanceId)
     {
-        var tcs = new TaskCompletionSource<ItemDetails>();
-        pendingRequests[instanceId] = tcs;
+        var url = $"https://api.steampowered.com/ISteamEconomy/GetAssetClassInfo/v1/?key={steamApiKey}&appid=730&class_count=1&classid0={instanceId}";
 
-        // Отправка запроса к Steam Game Coordinator
-        var msg = new ClientGCMsgProtobuf<CMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockRequest>((uint)ECsgoGCMsg.k_EMsgGCCStrike15_v2_Client2GCEconPreviewDataBlockRequest);
-        msg.Body.param_s = ulong.Parse(instanceId);
-
-        Console.WriteLine($"Sending request to GC for instanceId: {instanceId}");
-        gameCoordinator.Send(msg, APPID);
-
-        var timeoutTask = Task.Delay(15000); // 15 секунд таймаут
-        var completedTask = await Task.WhenAny(tcs.Task, timeoutTask);
-
-        if (completedTask == timeoutTask)
+        try
         {
-            Console.WriteLine("Request timed out.");
-            pendingRequests.TryRemove(instanceId, out _);
+            Console.WriteLine($"Fetching item details from URL: {url}");
+            var response = await httpClient.GetStringAsync(url);
+            Console.WriteLine($"Response from Steam API: {response}");
+            var json = JObject.Parse(response);
+            var item = json["result"]?[instanceId];
+
+            if (item == null)
+            {
+                Console.WriteLine("Item not found in response.");
+                return null;
+            }
+
+            var itemDetails = new ItemDetails
+            {
+                FloatValue = item["floatvalue"]?.Value<float>() ?? 0,
+                PaintSeed = item["paintseed"]?.Value<int>() ?? 0,
+                PaintIndex = item["paintindex"]?.Value<int>() ?? 0,
+                Stickers = item["stickers"]?.Select(s => new Sticker
+                {
+                    Name = s["name"]?.Value<string>() ?? string.Empty,
+                    Wear = s["wear"]?.Value<float>() ?? 0
+                }).ToArray()
+            };
+
+            return itemDetails;
+        }
+        catch (HttpRequestException httpEx)
+        {
+            Console.WriteLine($"HTTP request error: {httpEx.Message}");
             return null;
         }
-
-        return await tcs.Task;
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error fetching item details: {ex.Message}");
+            return null;
+        }
     }
 }
 
